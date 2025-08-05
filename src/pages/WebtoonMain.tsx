73ed20bf7def2826d72a28ecd17d5812f3d2c351
electrohyun <dev.electrohyun@gmail.com>
2025-08-05 15:49:55 +0900

    feat: (#9) 메인 페이지 퍼블리싱

--- src/pages/WebtoonMain.tsx
+++ src/pages/WebtoonMain.tsx
@@ -1,14 +1,58 @@
+import DaySection from "../components/DaySection";
+import type { DayOfWeek, MainSortOption, Webtoon } from "../types/webtoon";
+import { mockWebtoons } from "../mocks/models/webtoon";
+import { useState } from "react";
+
+const days: DayOfWeek[] = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
 function WebtoonMain() {
+  const [sort, setSort] = useState<MainSortOption>("like");
+
   return (
-    <>
-      <div className="max-w-[1190px] mx-auto px-2">
-        <div>
-          <br /><br /><br /><br /><br />
-          <p className="text-center">메인 페이지(공사중)</p>
-          <br /><br /><br /><br /><br />
-        </div>
+    <div className="mt-[25px]">
+      <div className="mb-2 text-sm flex items-center">
+        <span className="text-xl font-semibold">요일별 전체 웹툰</span>
+        <button 
+        onClick={() => setSort("like")} 
+        className={`ml-4 ${sort === "like" ? "text-site-red" : ""}`}
+        >
+          인기순
+        </button>
+        <button 
+        onClick={() => setSort("updated")} 
+        className={`ml-1 ${sort === "updated" ? "text-site-red" : ""}`}
+        >
+          &middot; 업데이트순
+        </button>
+        <button 
+        onClick={() => setSort("view")} 
+        className={`ml-1 ${sort === "view" ? "text-site-red" : ""}`}
+        >
+          &middot; 조회순
+        </button>
+        <button 
+        onClick={() => setSort("rate")} 
+        className={`ml-1 ${sort === "rate" ? "text-site-red" : ""}`}
+        >
+          &middot; 별점순
+        </button>
       </div>
-    </>
+      <main className="flex mt-[15px]">
+        {days.map((day) => {
+          const filtered = mockWebtoons.filter(
+            (webtoon: Webtoon) => webtoon.day_of_week === day
+          );
+
+          return (
+            <DaySection
+              key={day}
+              day={day}
+              webtoons={filtered}
+            />
+          );
+        })}
+      </main>
+    </div>
   );
 }
 
