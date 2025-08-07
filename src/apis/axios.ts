04ebc0ecf90e8d0cd2eb4d5377546e21d632fd60
electrohyun <dev.electrohyun@gmail.com>
2025-08-07 16:10:06 +0900

    feat: (#16) Axios 설정 및 인스턴스 2개 생성

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,37 +1,70 @@
-import axios from 'axios';
+import axios from "axios";
+import type { AxiosRequestConfig, AxiosError } from "axios";
+import { requestRefreshToken } from "@/apis/auth";
+import queryString from "query-string";
 
-const instance = axios.create({
+interface RetryConfig extends AxiosRequestConfig {
+  _retry?: boolean;
+}
+
+const paramsSerializer = (params: any) =>
+  queryString.stringify(params, { skipEmptyString: true, skipNull: true });
+
+const common: AxiosRequestConfig = {
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+  paramsSerializer,
+};
+
+export const publicAxios = axios.create(common);
+
+export const privateAxios = axios.create(common);
+
+privateAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  
+  return config;
 });
 
-instance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+privateAxios.interceptors.response.use(
+  (res) => res,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as RetryConfig;
 
-    if (
-      error.response?.status === 401 &&
-      !originalRequest.retry &&
-      !originalRequest.url.includes("/auth/token")
-    ) {
-      originalRequest.retry = true;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
 
       try {
-        const res = await instance.post("auth/token");
-        const newAccessToken = res.data.data.accessToken;
+        const res = await requestRefreshToken();
+        
+        if (!res.success) {
+          console.error(res.data.message);
+          return;
+        }
+        
+        const newAccessToken = res.data.accessToken;
 
         localStorage.setItem("accessToken", newAccessToken);
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-        return instance(originalRequest);
-      } catch (refreshError) {
-        console.error("토큰 재발급 실패", refreshError);
+        if (!originalRequest.headers) {
+          originalRequest.headers = {};
+        }
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken};`
+
+        return privateAxios(originalRequest);
+      } catch (err) {
+        console.error("리프레시 실패");
+
+        localStorage.clear();
+
         window.location.href = "/login";
+
+        return Promise.reject(err);
       }
     }
 
     return Promise.reject(error);
   }
 );
-
-export default instance;
