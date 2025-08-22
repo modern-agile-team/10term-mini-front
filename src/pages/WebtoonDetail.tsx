505373bdfa54d182f41020e165132a08f576cc79
electrohyun <dev.electrohyun@gmail.com>
2025-08-22 11:21:06 +0900

    refactor: (#24) 웹툰 상세 페이지 훅 분리 적용

--- src/pages/WebtoonDetail.tsx
+++ src/pages/WebtoonDetail.tsx
@@ -1,116 +1,29 @@
-import { instance } from '@/apis/axios';
 import { formatDateShort } from '@/utils/date';
 import { CheckIcon, PlusIcon, ShareIcon } from '@heroicons/react/24/outline';
 import { StarIcon } from '@heroicons/react/24/solid';
-import { useState, useEffect } from 'react';
-import { useSearchParams, useParams } from 'react-router';
-
-const largeAdImages = [
-  '/advertisements/large/adidas_large.webp',
-  '/advertisements/large/exhibition_large.webp',
-  '/advertisements/large/movie_large.webp',
-  '/advertisements/large/pocari_large.webp',
-];
-
-const smallAdImages = [
-  '/advertisements/small/adidas_small.webp',
-  '/advertisements/small/camera_small1.webp',
-  '/advertisements/small/camera_small2.webp',
-  '/advertisements/small/exibition_small.webp',
-  '/advertisements/small/movie_small.webp',
-  '/advertisements/small/pocari_small.webp',
-  '/advertisements/small/rehab_small.webp',
-  '/advertisements/small/rehab_small1.webp',
-];
-
-interface WebtoonDetailInfo {
-  id: number;
-  title: string;
-  writer: string;
-  illustrator: string;
-  weekdays: string[];
-  ageRating: string;
-  description: string;
-  thumbnailUrl: string;
-  favoriteCount: number;
-}
-
-interface WebtoonEpisode {
-  id: number;
-  episodeNo: number;
-  title: string;
-  thumbnailUrl: string;
-  postedTime: string;
-  ratingAvg: number;
-}
+import { useParams } from 'react-router';
+import { useWebtoonDetail } from '@/hooks/useWebtoonDetail';
 
 function WebtoonDetail() {
-  const [, setSearchParams] = useSearchParams();
-  const [webtoonDetail, setWebtoonDetail] = useState<WebtoonDetailInfo | null>(null);
-  const [error, setError] = useState<string>('');
-  const [isFavorite, setIsFavorite] = useState(false);
-  const [episodes, setEpisodes] = useState<WebtoonEpisode[]>([]);
-  const [randomLarge, setRandomLarge] = useState<string>('');
-  const [randomSmall, setRandomSmall] = useState<string>('');
-
   const { id } = useParams();
   const webtoonId = Number(id);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [webtoonResponse, episodesResponse] = await Promise.all([
-          instance.get<WebtoonDetailInfo>(`webtoons/${webtoonId}`),
-          instance.get(`webtoons/${webtoonId}/episodes`),
-        ]);
-        setWebtoonDetail(webtoonResponse.data.data.content);
-        setEpisodes(episodesResponse.data.data.content);
-
-        if (webtoonResponse.data.data.content.weekdays[0]) {
-          setSearchParams({ day: webtoonResponse.data.data.content.weekdays[0] });
-        }
-      } catch (err) {
-        setError('Failed to fetch data');
-      }
-    };
-
-    fetchData();
-  }, [webtoonId]);
-
-  useEffect(() => {
-    setRandomLarge(getRandomImage(largeAdImages));
-    setRandomSmall(getRandomImage(smallAdImages));
-  }, []);
-
-  const handleFavorite = async () => {
-    try {
-      const username = localStorage.getItem('user');
-      if (!username) {
-        alert('로그인이 필요합니다.');
-        return;
-      }
-
-      await instance.post(`webtoon/${webtoonId}/favorite`, { username });
-      setIsFavorite(!isFavorite);
-    } catch (err) {
-      console.error('Failed to update favorite status:', err);
-    }
-  };
-
-  console.log(episodes);
+  const {
+    webtoonDetail,
+    error,
+    isFavorite,
+    episodes,
+    randomAdvertisementLarge,
+    randomAdvertisementSmall,
+    handleFavorite,
+  } = useWebtoonDetail(webtoonId);
 
   if (error) return <div>Error: {error}</div>;
   if (!webtoonDetail) return <div>Loading...</div>;
 
-  function getRandomImage(images: string[]): string {
-    const index = Math.floor(Math.random() * images.length);
-    return images[index];
-  }
-
   return (
     <div className="flex justify-between">
       <div className="w-2/3">
-        {/* 웹툰 메타 카드 */}
+        {/* 웹툰 메타 정보 */}
         <div className="flex my-5 space-x-4">
           <img
             src={webtoonDetail.thumbnailUrl}
@@ -139,7 +52,7 @@ function WebtoonDetail() {
               {isFavorite ? (
                 <>
                   <CheckIcon className="w-5 h-5 mr-2" />
-                  관심 {webtoonDetail.favoriteCount + 1}
+                  관심 {webtoonDetail.favoriteCount}
                 </>
               ) : (
                 <>
@@ -183,8 +96,8 @@ function WebtoonDetail() {
         ))}
       </div>
       <div className="mt-20 w-1/4 space-y-4">
-        <img src={randomLarge} alt="대형 광고" />
-        <img src={randomSmall} alt="대형 광고" />
+        <img src={randomAdvertisementLarge} alt="대형 광고" />
+        <img src={randomAdvertisementSmall} alt="소형 광고" />
       </div>
     </div>
   );
