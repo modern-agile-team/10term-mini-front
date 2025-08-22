12700375c00d5b81a97f74ac2bda0298c4652262
electrohyun <dev.electrohyun@gmail.com>
2025-08-22 10:26:05 +0900

    feat: (#24) 광고 경로 상수 추가

---
+++ src/constants/advertisement.constants.ts
@@ -0,0 +1,17 @@
+export const largeAdvertisementImages = [
+  '/advertisements/large/adidas_large.webp',
+  '/advertisements/large/exhibition_large.webp',
+  '/advertisements/large/movie_large.webp',
+  '/advertisements/large/pocari_large.webp',
+] as const;
+
+export const smallAdvertisementImages = [
+  '/advertisements/small/adidas_small.webp',
+  '/advertisements/small/camera_small1.webp',
+  '/advertisements/small/camera_small2.webp',
+  '/advertisements/small/exibition_small.webp',
+  '/advertisements/small/movie_small.webp',
+  '/advertisements/small/pocari_small.webp',
+  '/advertisements/small/rehab_small.webp',
+  '/advertisements/small/rehab_small1.webp',
+] as const;
