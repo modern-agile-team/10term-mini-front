26ffe58bdb5ee265abcef4665105e60dec057df4
electrohyun <dev.electrohyun@gmail.com>
2025-08-25 11:53:09 +0900

    refactor: (#24) WebtoonId 타입 분리

--- src/apis/webtoonDetail.ts
+++ src/apis/webtoonDetail.ts
@@ -2,26 +2,28 @@ import type { ApiResponse } from '@/types/api';
 import { instance } from './axios';
 import type { WebtoonDetailInfo, WebtoonEpisode } from '@/types/webtoonDetail';
 
+type WebtoonId = number;
+
 export type WebtoonDetailResponse = ApiResponse<WebtoonDetailInfo>;
 export type WebtoonEpisodeResponse = ApiResponse<WebtoonEpisode[]>;
 export type FavoriteResponse = ApiResponse<{ success: boolean }>;
 
-export const requestWebtoonDetail = async (webtoonId: number) => {
+export const requestWebtoonDetail = async (webtoonId: WebtoonId) => {
   const response = await instance.get<WebtoonDetailResponse>(`webtoons/${webtoonId}`);
   return response.data.data.content;
 };
 
-export const requestWebtoonEpisodes = async (webtoonId: number) => {
+export const requestWebtoonEpisodes = async (webtoonId: WebtoonId) => {
   const response = await instance.get<WebtoonEpisodeResponse>(`webtoons/${webtoonId}/episodes`);
   return response.data.data.content;
 };
 
-export const requestAddFavorite = async (webtoonId: number) => {
+export const requestAddFavorite = async (webtoonId: WebtoonId) => {
   const response = await instance.post<FavoriteResponse>(`webtoons/${webtoonId}/favorite`);
   return response.data.data.content;
 };
 
-export const requestRemoveFavorite = async (webtoonId: number) => {
+export const requestRemoveFavorite = async (webtoonId: WebtoonId) => {
   const response = await instance.delete<FavoriteResponse>(`webtoons/${webtoonId}/favorite`);
   return response.data.data.content;
 };
