896b3620242415424cf02c6cf728db15bb0c0890
electrohyun <dev.electrohyun@gmail.com>
2025-08-25 16:42:35 +0900

    refactor: (#23) form 태그 사용 및 상수 컴포넌트 외부로 분리

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -12,6 +12,8 @@ interface HeaderProps {
   } | null;
 }
 
+const EXCLUDED_PATHS = ['/favorites', '/mypage', '/search'] as const;
+
 function Header({ user }: HeaderProps) {
   const navigate = useNavigate();
 
@@ -46,7 +48,6 @@ function Header({ user }: HeaderProps) {
   const [searchParams, setSearchParams] = useSearchParams();
   const selectedDay = searchParams.get('day');
 
-  const EXCLUDED_PATHS = ['/favorites', '/mypage', '/search'];
   const location = useLocation();
 
   return (
@@ -62,33 +63,34 @@ function Header({ user }: HeaderProps) {
           </Link>
         </h1>
         <div className="flex items-center justify-between">
-          <div
+          <form
+            role="search"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSearch();
+            }}
             className="flex items-center border my-[14px] mr-[18px] gap-x-2
-          group focus-within:outline focus-within:outline-2 focus-within:outline-blue-500 rounded-sm"
+    group focus-within:outline focus-within:outline-2 focus-within:outline-blue-500 rounded-sm"
           >
             <input
               type="text"
               value={inputValue}
               onChange={handleInputValue}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') handleSearch();
-              }}
               placeholder="제목/작가로 검색할 수 있습니다."
               maxLength={18}
-              className="w-60 pl-3 h-9 text-base
-            focus:outline-none"
+              className="w-60 pl-3 h-9 text-base focus:outline-none"
             />
             <button
+              type="button"
               onClick={() => setInputValue('')}
               className={inputValue ? '' : 'opacity-0 pointer-events-none'}
             >
               <XCircleIcon className="w-5 h-5 text-gray-500 cursor-pointer" />
             </button>
-            <button onClick={handleSearch}>
+            <button type="submit">
               <MagnifyingGlassIcon className="w-5 h-5 text-gray-500" />
             </button>
-            &nbsp;&nbsp;
-          </div>
+          </form>
           {user ? (
             <div className="flex items-center gap-3">
               <span className="text-sm text-gray-600">{user.nickname}님</span>
