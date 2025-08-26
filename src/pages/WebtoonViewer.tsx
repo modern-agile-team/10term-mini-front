bee87c80e6189df74620d2eb19953a999b0b4643
electrohyun <dev.electrohyun@gmail.com>
2025-08-26 10:29:47 +0900

    feat: (#25) 웹툰 뷰어 헤더 추가

--- src/pages/WebtoonViewer.tsx
+++ src/pages/WebtoonViewer.tsx
@@ -1,8 +1,16 @@
+import WebtoonViewerHeader from '@/components/WebtoonViewerHeader';
+
 export default function WebtoonViewer() {
   return (
-    <div>
-      <h1>Webtoon Viewer</h1>
-      {/* Add your webtoon viewing logic here */}
-    </div>
+    <>
+      <WebtoonViewerHeader
+        webtoonTitle="웹툰 이름"
+        episodeNo={123}
+        episodeTitle="테스트 에피소드 제목"
+      />
+      <div className="mt-10 justify-center items-center flex">
+        <img src="https://i.ibb.co/Kzb11gWP/1-1.webp" />
+      </div>
+    </>
   );
 }
