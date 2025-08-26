f7bb896e9dd970eaec7d1f7b5929778bbf1ba94c
electrohyun <dev.electrohyun@gmail.com>
2025-08-26 11:02:57 +0900

    feat: (#25) 헤더, 본문 mock 데이터 삽입

--- src/pages/WebtoonViewer.tsx
+++ src/pages/WebtoonViewer.tsx
@@ -2,15 +2,15 @@ import WebtoonViewerHeader from '@/components/WebtoonViewerHeader';
 
 export default function WebtoonViewer() {
   return (
-    <>
+    <div className="">
       <WebtoonViewerHeader
         webtoonTitle="웹툰 이름"
         episodeNo={123}
         episodeTitle="테스트 에피소드 제목"
       />
-      <div className="mt-10 justify-center items-center flex">
+      <div className="w-[1190px] mx-auto mt-10 justify-center items-center flex">
         <img src="https://i.ibb.co/Kzb11gWP/1-1.webp" />
       </div>
-    </>
+    </div>
   );
 }
