6bf277d51fb0bea37d25d79d01c7d84ec986adba
electrohyun <dev.electrohyun@gmail.com>
2025-09-01 11:35:40 +0900

    refactor: (#26) 타입 추상화 해제

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -1,5 +1,3 @@
-// /apis/comment.ts
-
 import { instance } from '@/apis/axios';
 import type { ApiResponse } from '@/types/api';
 import type {
@@ -9,18 +7,8 @@ import type {
   Comment,
 } from '@/types/comment';
 
-type CommentApiResponse = ApiResponse<CommentContent>;
-type CreateCommentApiResponse = ApiResponse<NewCommentResponse>;
-type CommentReactionApiResponse = ApiResponse<{
-  myReaction: 'like' | 'dislike' | null;
-}>;
-type UpdateCommentApiResponse = ApiResponse<Comment>;
-type DeleteCommentApiResponse = ApiResponse<{
-  message: string;
-}>;
-
 export const requestGetComments = async (episodeId: number): Promise<CommentContent> => {
-  const res = await instance.get<CommentApiResponse>(`episodes/${episodeId}/comments`);
+  const res = await instance.get<ApiResponse<CommentContent>>(`episodes/${episodeId}/comments`);
   return res.data.data.content;
 };
 
@@ -28,7 +16,10 @@ export const requestCreateComment = async (
   episodeId: number,
   data: NewCommentRequest,
 ): Promise<NewCommentResponse> => {
-  const res = await instance.post<CreateCommentApiResponse>(`episodes/${episodeId}/comments`, data);
+  const res = await instance.post<ApiResponse<NewCommentResponse>>(
+    `episodes/${episodeId}/comments`,
+    data,
+  );
   return res.data.data.content;
 };
 
@@ -36,23 +27,23 @@ export const requestToggleCommentReaction = async (
   commentId: number,
   type: 'like' | 'dislike' | null,
 ): Promise<{ myReaction: 'like' | 'dislike' | null }> => {
-  const res = await instance.put<CommentReactionApiResponse>(`comments/${commentId}/reaction`, {
-    type,
-  });
+  const res = await instance.put<ApiResponse<{ myReaction: 'like' | 'dislike' | null }>>(
+    `comments/${commentId}/reaction`,
+    { type },
+  );
+
   return res.data.data.content;
 };
 
 export const requestUpdateComment = async (
   commentId: number,
   content: string,
-): Promise<UpdateCommentApiResponse['data']['content']> => {
-  const res = await instance.patch<UpdateCommentApiResponse>(`comments/${commentId}`, {
-    content,
-  });
+): Promise<Comment> => {
+  const res = await instance.patch<ApiResponse<Comment>>(`comments/${commentId}`, { content });
   return res.data.data.content;
 };
 
 export const requestDeleteComment = async (commentId: number): Promise<string | undefined> => {
-  const res = await instance.delete<DeleteCommentApiResponse>(`comments/${commentId}`);
+  const res = await instance.delete<ApiResponse<{ message: string }>>(`comments/${commentId}`);
   return res.data.data.message;
 };
